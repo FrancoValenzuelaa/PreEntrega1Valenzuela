60c8190920666bbb330313b76b2f1f832e91b484
francovalenzuelaa <valenzuelafranco442@gmail.com>
2023-08-10 23:50:16 -0300

    se modificaron los archivos

--- JS/carrito.js
+++ JS/carrito.js
@@ -1,21 +1,11 @@
-const producto = [
-    {codigo: 1, nombre: "pre-workout", precio: 50 },
-    {codigo: 2, nombre: "bcaa", precio: 50 },
-    {codigo: 3, nombre: "proteina", precio: 55 },
-    {codigo: 4, nombre: "while protein", precio: 70 },
-    {codigo: 5, nombre: "gold protein", precio: 90 },
-    {codigo: 6, nombre: "creatina", precio: 150 },
-]
-
-function buscarProducto(codigo){
-    let resultado = productos.find((producto)=> producto.codigo === parseInt(codigo))
-    return resultado
-}
 
 let carrito = [];
 
 producto.forEach( (product) =>  {
     let content = document.createElement("div");
     content.innerHTML = `
-    `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p>${product.precio} $</p>
+    `;
 })
---
+++ JS/products.js
@@ -0,0 +1,38 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Creatina",
+        precio: 8500,
+        img:
+    },
+    {
+        id: 2,
+        nombre: "Whey Protein",
+        precio: 10700,
+        img:
+    }
+    {
+        id: 2,
+        nombre: "Whey Protein",
+        precio: 10700,
+        img:
+    }
+    {
+        id: 2,
+        nombre: "Whey Protein",
+        precio: 10700,
+        img:
+    }
+    {
+        id: 2,
+        nombre: "Whey Protein",
+        precio: 10700,
+        img:
+    }
+    {
+        id: 2,
+        nombre: "Whey Protein",
+        precio: 10700,
+        img:
+    }
+]
